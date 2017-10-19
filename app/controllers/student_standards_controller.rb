4c26d6b8829c1bb5d93a72a31f4f3f99bcffb1ee
Isaac Orme <isorme1@gmail.com>
2017-10-18 22:19:14 -0700

    ajax-edit-update: attempt 1

--- app/controllers/student_standards_controller.rb
+++ app/controllers/student_standards_controller.rb
@@ -3,19 +3,32 @@ class StudentStandardsController < ApplicationController
   def edit
     @student = Student.find(params[:student_id])
     @student_standard = @student.student_standards.find(params[:id])
+
+    respond_to do |format|
+      format.html {redirect_to @student_standard}
+      format.js
+    end
   end
 
   def update
-    @student = Student.find(params[:student_id])
+    @student = Student.find(params[:student_id])  #changed this from student_id to id
     @student_standard = @student.student_standards.find(params[:id])
 
-    if @student_standard.update(student_standard_params)
-      flash[:notice] =  "Standard has been updated"
-      redirect_to student_path(@student, anchor: @student_standard)
-    else
-      flash[:error] = "Standard was not updated. Please try again."
-      render 'edit'
+    respond_to do |format|
+      if @student_standard.update(student_standard_params)
+        format.html { redirect_to @student_standard }
+        format.js
+      else
+        format.html { render :edit }
+      end
     end
+    #if @student_standard.update(student_standard_params)
+      #flash[:notice] =  "Standard has been updated"
+      #redirect_to student_path(@student, anchor: @student_standard)
+    #else
+      #flash[:error] = "Standard was not updated. Please try again."
+      #render 'edit'
+    #end
   end
 
   def create
